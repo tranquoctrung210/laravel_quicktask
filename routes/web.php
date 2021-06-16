7889f977012b91679f9c0938adae4f291571f83c
TrungTran <quoctrung.tran210@gmail.com>
2021-06-16 19:59:33 +0700

    Done view/add/delete todo list

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'locale'], function() {
+    Route::get('change-language/{language}', 'ChangeLanguageController@changeLanguage')
+        ->name('user.change-language');
 });
+
+Route::resource('tasks', 'TaskController');
